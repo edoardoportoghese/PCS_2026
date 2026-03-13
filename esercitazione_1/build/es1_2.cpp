f37c8889c2a60c7372553daaf9b0dc5b9f1982b9
edoardoportoghese <edoardoportoghese@gmail.com>
2026-03-13 14:08:40 +0000

    <esercitazione_1>

---
+++ esercitazione_1/build/es1_2.cpp
@@ -0,0 +1,35 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+string filename = argv[1];
+ifstream ifs(argv[1]);
+if ( ifs.is_open() ) { // Check if file successfully opened
+int n=4;
+while( !ifs.eof() ) {
+string location;
+ifs >> location;
+if (ifs.fail()) break;
+// read temperatur
+ double temp = 0;
+for (int i=0; i<n; i++) {
+double record;
+ifs >> record;
+temp += record;
+}
+temp = temp/4.0;
+
+//stampo risultati
+cout << location << "  " << temp << "\n";
+}
+ifs.close(); // Optional: ofstream is RAII
+return 0;
+}
+else {
+cout << "error opening file \n";
+return 1;
+}
+
+}
